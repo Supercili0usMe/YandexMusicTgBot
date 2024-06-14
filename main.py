fc0fbedb68e2a415c1b36daba5342f50313f9451
Supercili0usMe <supercilious_me@vk.com>
2024-06-14 15:18:19 +0300

    Delete RickRoll

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from telebot import TeleBot
-import webbrowser
+from telebot import types
 from bot.config import TELEGRAM_BOT_TOKEN
 
 # Чтение из конфигурационного файла
 bot = TeleBot(TELEGRAM_BOT_TOKEN)
 
+# Обработка файлов
+@bot.message_handler(content_types=["photo"])
+def get_photo(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Перейди на сайт!", url="https://youtu.be/dQw4w9WgXcQ")
+    markup.row(btn1)
+    btn2 = types.InlineKeyboardButton("Удалить фото", callback_data="delete")
+    btn3 = types.InlineKeyboardButton("Изменить текст", callback_data="edit")
+    markup.row(btn2, btn3)
+    bot.reply_to(message, "Красивое...", reply_markup=markup)
+
 # Обработка стартовых команд 
 @bot.message_handler(commands=["start", 'main', 'hello'])
 def answer(message):
-    bot.send_message(message.chat.id, f"Привет, {message.from_user.username}!")
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Перейди на сайт!", url="https://youtu.be/dQw4w9WgXcQ")
+    markup.row(btn1)
+    btn2 = types.InlineKeyboardButton("Удалить фото", callback_data="delete")
+    btn3 = types.InlineKeyboardButton("Изменить текст", callback_data="edit")
+    markup.row(btn2, btn3)
+    bot.reply_to(message, f"Привет, {message.from_user.username}!", reply_markup=markup)
 
 # Обработка команды help
 @bot.message_handler(commands=["help"])
@@ -19,11 +36,6 @@ def answer(message):
 @bot.message_handler(commands=["info"])
 def answer(message):
     bot.send_message(message.chat.id, message)    
-
-# Обработка команды site или website
-@bot.message_handler(commands=["site", "website"])
-def site(message):
-    webbrowser.open("https://youtu.be/dQw4w9WgXcQ?si=pQXHdXpZquPp3WKY")
 
 #Обработка любого текста, введённого пользователем
 @bot.message_handler()
